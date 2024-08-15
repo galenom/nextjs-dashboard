2a8b45eb0d72d9d1663d5d1c50d1b51255d87be0
galenom <galeno.mario@gmail.com>
2024-08-14 21:25:23 -0400

    feat: add titles to layout and invoices page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,13 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
 
 export const experimental_ppr = true;
+export const metadata: Metadata = {
+  title: 'Acme Dashboard',
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 
 export default function RootLayout({
   children,
@@ -10,9 +16,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <head>
-        <title>Tutorial</title>
-      </head>
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
